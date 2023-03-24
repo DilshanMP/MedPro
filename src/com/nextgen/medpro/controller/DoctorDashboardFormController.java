c0f11d2167675143725c43c85d897793941f89e5
DilshanMP <127347083+DilshanMP@users.noreply.github.com>
2023-03-24 15:13:53 +0530

    DoctorDashboardForm.fxml,DoctorRegister.fxml , & Controller

---
+++ src/com/nextgen/medpro/controller/DoctorDashboardFormController.java
@@ -0,0 +1,18 @@
+package com.nextgen.medpro.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class DoctorDashboardFormController {
+    public AnchorPane doctorDashboardContext;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void navigateToPatientManagementPage(MouseEvent mouseEvent) {
+    }
+
+    public void logoutOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/com/nextgen/medpro/controller/DoctorRegistrationFormController.java
+++ src/com/nextgen/medpro/controller/DoctorRegistrationFormController.java
@@ -1,4 +1,22 @@
 package com.nextgen.medpro.controller;
 
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DoctorRegistrationFormController {
+    public AnchorPane doctorRegistrationContext;
+    public JFXTextField txtFirstName;
+    public JFXTextField txtLastName;
+    public JFXTextField txtEmail;
+    public JFXRadioButton rBtnMale;
+    public JFXTextField txtNic;
+    public JFXTextField txtContact;
+    public JFXTextField txtSpecializations;
+    public JFXTextArea txtAddress;
+
+    public void signUpOnAction(ActionEvent actionEvent) {
+    }
 }
